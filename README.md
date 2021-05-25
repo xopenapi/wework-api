ed88f89bacbb2d6c159f46a60fb8b670851b9d70
fioman <fioman@qq.com>
2021-05-26 01:36:41 +0800

    autorest

---
+++ README.md
@@ -0,0 +1,15 @@
+# 企业微信API
+
+## 生成SDK
+
+* 安装`autorest`
+
+```bash
+npm i autorest -g
+```
+
+* 生成 SDK 代码
+
+```bash
+autorest autorest.yaml
+```
--- generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run --rm -v "${PWD}:/local" xopenapi/openapi-generator-cli generate \
-    --git-user-id xopenapi \
-    --git-repo-id wework-api-go \
-    --package-name wework \
-    -i /local/wework.yaml \
-    -g go \
-    -o /local/out/go
